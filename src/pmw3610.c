70b900f847e1fb93e820cacf6a9e74bb881202a3
mmmtmi <minamii.tks@gmail.com>
2024-11-17 17:12:21 +0900

    pmw3610.c を更新

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -585,7 +585,10 @@ static int pmw3610_report_data(const struct device *dev) {
         LOG_WRN("Device is not initialized yet");
         return -EBUSY;
     }
-    
+if( x + y < 1)
+{
+    break;
+}
     int32_t dividor;
     enum pixart_input_mode input_mode = get_input_mode_for_current_layer(dev);
     bool input_mode_changed = data->curr_mode != input_mode;
